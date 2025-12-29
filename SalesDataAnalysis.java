0bf9e5ecdd761bf9bf91cc63f567e3e7faf366bd
juhina923-oss <juhina923@gmail.com>
2025-12-29 23:38:01 +0400

    Create SalesDataAnalysis.java
    this master file

---
+++ SalesDataAnalysis.java
@@ -0,0 +1,19 @@
+public class SalesDataAnalysis {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        for (int i = 1; i <= 3; i++) {
+            System.out.println("Product " + i);
+        }
+        System.out.println("Enter product sales level:");
+        int choice = input.nextInt();
+        if (choice == 1) {
+            System.out.println("Low Sales Product");
+        }
+        else if (choice == 2) {
+            System.out.println("Best-Selling Product");
+        }
+        else {
+            System.out.println("Invalid sales value entered");
+        }
+    }
+}
